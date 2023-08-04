d5f1223236ba437dd6729f23fc2dffa82975b926
Yuri Duarte <yuriduartedev@gmail.com>
2023-08-04 14:14:44 -0400

    setup coverage and ui tests

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -45,7 +45,7 @@ describe("Register Use Case", () => {
       password: "123456",
     });
 
-    expect(
+    await expect(
       async () =>
         await registerUseCase.execute({
           name: "John Doe",
